6ec180c5acc4d39855f87ce6717243887b195399
Hakune <69071025+Hakune027@users.noreply.github.com>
2026-06-16 21:27:35 +0800

    修复托盘右键菜单无法打开

--- TaskbarLyrics.App/TrayService.cs
+++ TaskbarLyrics.App/TrayService.cs
@@ -8,27 +8,38 @@ public sealed class TrayService : IDisposable
 {
     private readonly Icon _icon;
     private readonly Forms.NotifyIcon _notifyIcon;
+    private readonly Forms.ContextMenuStrip _contextMenu;
     private TrayMenuWindow? _menuWindow;
 
     public TrayService(Action toggleLyricsWindow, Action openSettings, Action exitApp)
     {
         _icon = AppIconProvider.LoadTrayIcon();
+
+        _contextMenu = new Forms.ContextMenuStrip();
+
+        var toggleItem = new Forms.ToolStripMenuItem("显示/隐藏歌词");
+        toggleItem.Click += (_, _) => toggleLyricsWindow();
+        _contextMenu.Items.Add(toggleItem);
+
+        var settingsItem = new Forms.ToolStripMenuItem("设置");
+        settingsItem.Click += (_, _) => openSettings();
+        _contextMenu.Items.Add(settingsItem);
+
+        _contextMenu.Items.Add(new Forms.ToolStripSeparator());
+
+        var exitItem = new Forms.ToolStripMenuItem("退出");
+        exitItem.Click += (_, _) => exitApp();
+        _contextMenu.Items.Add(exitItem);
+
         _notifyIcon = new Forms.NotifyIcon
         {
             Text = "TaskbarLyrics",
             Icon = _icon,
-            Visible = true
+            Visible = true,
+            ContextMenuStrip = _contextMenu
         };
 
         _notifyIcon.DoubleClick += (_, _) => toggleLyricsWindow();
-        _notifyIcon.MouseUp += (_, e) =>
-        {
-            if (e.Button == Forms.MouseButtons.Right)
-            {
-                System.Windows.Application.Current.Dispatcher.BeginInvoke(() =>
-                    ShowMenu(toggleLyricsWindow, openSettings, exitApp));
-            }
-        };
     }
 
     public void Dispose()
@@ -36,6 +47,7 @@ public sealed class TrayService : IDisposable
         _notifyIcon.Visible = false;
         _notifyIcon.Dispose();
         _menuWindow?.Close();
+        _contextMenu.Dispose();
         _icon.Dispose();
     }
 
